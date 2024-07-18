cae471def92e8cd5c6f799a879f1169c38b3793b
FranciscoCTI <franciscofcch@gmail.com>
2024-07-17 22:42:05 -0400

    #Renamed page #Fixed connection to mongoose

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-/*if(process.env.NODE_ENV !== 'production')
+if(process.env.NODE_ENV !== 'production')
     {
-        require('dotenv').parse()
-    }*/
+        require('dotenv').config({path:'.env'})
+    }
 
 const express= require('express')
 const app = express()
@@ -15,13 +15,11 @@ app.set('layout', 'layouts/alternateLayout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-/*
 const mongoose=require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', error=>console.error(error))
 db.once('open', () =>console.log('connected to mongoose'))
-*/
 
 app.use('/',indexRouter)
 
